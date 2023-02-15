8874a8ac6055db9dbeb7c8ec763b76dd927db5f5
Peter Hofmann <contact@firstfirst.design>
2023-02-15 13:11:17 +0100

    start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,35 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
     title: `Naturheilpraxis Mitter`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.naturheilpraxis-mitter.ch`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
-    __key: "images"
-  }]
+
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
 };
---
+++ src/pages/ueber-mich.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const UeberMich = () => {
+  return (
+    <Layout>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  );
+};
+
+export default UeberMich;
+
+export const Head = () => <title>Über mich</title>;
